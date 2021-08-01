88502b5d90591ae228ec70a468b912ec9c19a5a9
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-01 18:29:33 -0500

    add db to config object and create flag for db.dsn, and start db client

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/cmd-ctrl-q/go-movies-server/models"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string // db connection string
+	}
 }
 
 type AppStatus struct {
@@ -26,6 +34,7 @@ type AppStatus struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models models.Models
 }
 
 func main() {
@@ -33,14 +42,22 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://plutonium@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
 	// log.Ldate|log.Ltime add date and time to logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: models.NewModels(db),
 	}
 
 	srv := &http.Server{
@@ -53,8 +70,27 @@ func main() {
 
 	logger.Println("Starting server on port", cfg.port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// open conn to db
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// ping db
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
